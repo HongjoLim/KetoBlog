ad8d6a5acc03bf3120b7fb7651e33db6659f3c86
Hongjo Lim <hongjolim@geotab.com>
2021-12-07 19:10:49 -0800

    Used promise

--- index.js
+++ index.js
@@ -7,30 +7,37 @@ const mongoose = require('mongoose');
 const models = require('./models/Models');
 require('dotenv').config();
 
+mongoose.Promise = Promise;
+
 const DB_URL = process.env.MONGOLAB_URI;
 
 app.use(express.static(__dirname));
 app.use(bp.json());
 app.use(bp.urlencoded({extended: false}));
 
-const postings = [];
-
 app.get('/postings', (req, res) => {
-    res.send(postings);
-})
+    models.Posting.find({}, (err, postings) => {
+        if (err)
+            console.log(err);
+
+        res.send(postings);
+    });
+});
 
 app.post('/postings', (req, res) => {
     let posting = models.Posting(req.body);
+    console.log(posting);
 
-    posting.save(err => {
-        if (err)
-            sendStatus(500);
-        else {
-            socket.emit('post', req.body);
-            res.sendStatus(200);
-        }
+    posting.save()
+    .then(() => {
+        socket.emit('post', req.body);
+        res.sendStatus(200);
+    })
+    .catch(err => {
+        res.sendStatus(500);
+        console.log(err);
     });
-})
+});
 
 socket.on('connection', (so) => {
     console.log('user connected!');
